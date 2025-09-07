492c7f1994c8ca3b0f9383ccf04911f1513db324
utled <johansson.fre@gmail.com>
2025-09-07 18:47:43 +0200

    add os independent terminal clearing

---
+++ home_helpers/terminal_clearer.go
@@ -0,0 +1,34 @@
+package home_helpers
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+var clearFunctions map[string]func()
+
+func InitClearFunctions() {
+	clearFunctions = make(map[string]func())
+	clearFunctions["linux"] = func() {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clearFunctions["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+func ClearTerminal() error {
+	clearFunction, ok := clearFunctions[runtime.GOOS]
+	if ok {
+		clearFunction()
+	} else {
+		return fmt.Errorf("unable to clear the terminal")
+	}
+	return nil
+}
