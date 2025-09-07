85edda8895d39d6c50d11d311486ed67574a6a56
utled <johansson.fre@gmail.com>
2025-09-07 13:27:44 +0200

    implement base quering

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"fTime/cli"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	signalChan := make(chan os.Signal, 1)
+	exitChan := make(chan struct{})
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
 	err := cli.Main()
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 	}
+
+	go func() {
+		<-signalChan
+		close(exitChan)
+	}()
 }
