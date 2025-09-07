44934e6bf64b88d7f0bfe32b2a0e32b908a75641
utled <johansson.fre@gmail.com>
2025-09-07 18:44:27 +0200

    implement signal listening to enable controlled shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -10,16 +10,17 @@ import (
 
 func main() {
 	signalChan := make(chan os.Signal, 1)
-	exitChan := make(chan struct{})
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-signalChan
+		fmt.Println()
+		os.Exit(0)
+	}()
 
 	err := cli.Main()
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 	}
 
-	go func() {
-		<-signalChan
-		close(exitChan)
-	}()
 }
