77db89df47bdbe186af47afb043d15564bc2d7a9
utled <johansson.fre@gmail.com>
2025-09-12 11:54:05 +0200

    fix int16 convertion formatting

--- actions/lunch.go
+++ actions/lunch.go
@@ -11,7 +11,7 @@ func RegisterLunch(lunchString string, state *helpers.ReportState) error {
 	if err != nil {
 		return fmt.Errorf("failed to convert lunch duration to numeric value.\nInput format must be <MM>")
 	}
-	var lunchDuration int16 = int16(lunchDurationInt)
+	lunchDuration := int16(lunchDurationInt)
 	if lunchDuration < 1 || lunchDuration > 59 {
 		return fmt.Errorf("lunch duration must be between 1 and 59 in format <MM>")
 	}
