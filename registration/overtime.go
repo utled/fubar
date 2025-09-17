4279ce9e65227b9deb4d2ec4413f31fbcd3a07b0
utled <johansson.fre@gmail.com>
2025-09-17 08:38:03 +0200

    add >8 hour restriction for overtime registration

--- registration/overtime.go
+++ registration/overtime.go
@@ -2,12 +2,25 @@ package registration
 
 import (
 	"fTime/data"
+	"fTime/utils"
 	"fmt"
+	"time"
 )
 
 func RegisterOvertime(otArgument string, state *data.ReportState, userConfig *data.UserConfig) error {
 	if !state.SelectedRecord.EndTime.Valid {
 		fmt.Println("end time must be registered before the day is flagged for overtime")
+	}
+
+	registeredDayTotal, err := time.Parse(utils.NonsenseDate, state.SelectedRecord.DayTotal.String)
+	if err != nil {
+		return fmt.Errorf("failed to parse day total: %v", err)
+	}
+	strippedOfEightHours := registeredDayTotal.Add(-8 * time.Hour)
+
+	referenceTime := time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)
+	if strippedOfEightHours.Before(referenceTime) || strippedOfEightHours.Equal(referenceTime) {
+		return fmt.Errorf("day total must be greater than 08:00 hours to register overtime")
 	}
 
 	if otArgument == "ot" {
@@ -27,7 +40,7 @@ func RegisterOvertime(otArgument string, state *data.ReportState, userConfig *da
 	}
 
 	endTime := state.SelectedRecord.EndTime.String[:2] + state.SelectedRecord.EndTime.String[3:5]
-	err := RegisterEnd(endTime, state, userConfig)
+	err = RegisterEnd(endTime, state, userConfig)
 	if err != nil {
 		return err
 	}
