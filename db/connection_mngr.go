e22008a73fb2d90e6600097711c2227229a1a8b9
utled <johansson.fre@gmail.com>
2025-09-25 10:18:57 +0200

    fix env variables for DB connection

--- db/connection_mngr.go
+++ db/connection_mngr.go
@@ -2,31 +2,24 @@ package db
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"github.com/go-sql-driver/mysql"
 	"os"
+
+	"github.com/go-sql-driver/mysql"
 )
 
-type credentials struct {
-	User string `json:"User"`
-	Pswd string `json:"Pswd"`
-	Host string `json:"Host"`
-}
-
 func CreateConnection() (db *sql.DB, err error) {
-	var dbCredentials credentials
-	credentialsJson := os.Getenv("FTIMEDB")
-	err = json.Unmarshal([]byte(credentialsJson), &dbCredentials)
-	if err != nil {
-		return db, err
-	}
+	dbUser := os.Getenv("FTIMEDBUser")
+	dbPswd := os.Getenv("FTIMEDBPswd")
+	dbHost := os.Getenv("FTIMEDBHost")
+
+	fmt.Println(dbUser, dbPswd, dbHost)
 
 	cfg := mysql.NewConfig()
-	cfg.User = dbCredentials.User
-	cfg.Passwd = dbCredentials.Pswd
+	cfg.User = dbUser
+	cfg.Passwd = dbPswd
 	cfg.Net = "tcp"
-	cfg.Addr = dbCredentials.Host
+	cfg.Addr = dbHost
 	cfg.DBName = "ftime"
 
 	db, err = sql.Open("mysql", cfg.FormatDSN())
