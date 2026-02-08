fc534e6e761f56335108eb8a8f9c6396ec93832f
Fredrik Johansson <johansson.fre@gmail.com>
2026-02-08 20:46:53 +0100

    refactor stats view

--- tui/view_stats.go
+++ tui/view_stats.go
@@ -6,11 +6,11 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-func (model *Model) renderStatisticsView() string {
+func (model *Model) statsHeaderView() string {
 	model.headerFields[0].SetValue("")
 	model.headerFields[0].Placeholder = "  [d] Daily"
 	model.headerFields[2].SetValue("    Stats")
-	header := lipgloss.JoinHorizontal(
+	return lipgloss.JoinHorizontal(
 		lipgloss.Center,
 		model.inputStyle.InputField.Render(model.headerFields[0].View()),
 		lipgloss.NewStyle().BorderForeground(lipgloss.Color("238")).BorderStyle(lipgloss.NormalBorder()).PaddingLeft(0).Render(model.headerFields[1].View()),
@@ -18,7 +18,18 @@ func (model *Model) renderStatisticsView() string {
 		model.inputStyle.InputField.Render(model.headerFields[3].View()),
 		model.inputStyle.InputField.Render(model.headerFields[4].View()),
 	)
+}
 
+func (model *Model) statsYearSelectionView() string {
+	return lipgloss.JoinHorizontal(
+		lipgloss.Center,
+		"<-[p] ",
+		model.inputStyle.InputField.PaddingRight(1).Render(model.statsDetails.yearSelection.View()),
+		" [n]->   ",
+	)
+}
+
+func (model *Model) allSumView() string {
 	allSumOne := lipgloss.JoinVertical(
 		lipgloss.Center,
 		lipgloss.JoinHorizontal(lipgloss.Left, fmt.Sprintf("%25s", "Worked Days: "), model.statsDetails.allSumFields[idxWorkedDays].View()),
@@ -40,69 +51,73 @@ func (model *Model) renderStatisticsView() string {
 		"",
 		lipgloss.JoinHorizontal(lipgloss.Left, fmt.Sprintf("%10s", "OT Days: "), model.statsDetails.allSumFields[idxOTDays].View()),
 	)
-	allSumFields := lipgloss.JoinHorizontal(
+	return lipgloss.JoinHorizontal(
 		lipgloss.Left,
 		allSumOne,
 		allSumTwo,
 		allSumThree,
 		"      ",
 	)
+}
 
-	monthSumFields := lipgloss.JoinHorizontal(
+func (model *Model) statsGraphView() string {
+		return lipgloss.JoinVertical(
+			lipgloss.Center,
+			model.statsDetails.graphArea.View(),
+			"\n",
+			lipgloss.NewStyle().Bold(true).Underline(true).Render("All time"),
+			model.allSumView(),
+		)
+}
+
+func (model *Model) monthSumView() string {
+	return lipgloss.JoinHorizontal(
 		lipgloss.Left,
 		lipgloss.JoinVertical(lipgloss.Left, lipgloss.NewStyle().Bold(true).Render(fmt.Sprintf("%-12s", "Avg Start")), model.statsDetails.monthSumFields[0].View()),
 		lipgloss.JoinVertical(lipgloss.Left, lipgloss.NewStyle().Bold(true).Render(fmt.Sprintf("%-12s", "Avg End")), model.statsDetails.monthSumFields[1].View()),
 		lipgloss.JoinVertical(lipgloss.Left, lipgloss.NewStyle().Bold(true).Render(fmt.Sprintf("%-12s", "Avg Lunch")), model.statsDetails.monthSumFields[2].View()),
 		lipgloss.JoinVertical(lipgloss.Left, lipgloss.NewStyle().Bold(true).Render(fmt.Sprintf("%-12s", "Avg OT")), model.statsDetails.monthSumFields[3].View()),
 	)
+}
 
-	yearSelection := lipgloss.JoinHorizontal(
-		lipgloss.Center,
-		"<-[p] ",
-		model.inputStyle.InputField.PaddingRight(1).Render(model.statsDetails.yearSelection.View()),
-		" [n]->   ",
-	)
-	topSpacing := "\n\n"
+func (model *Model) statsTableView() string {
+		return lipgloss.JoinVertical(
+			lipgloss.Center,
+			model.statsDetails.table.View(),
+			"\n\n\n\n\n\n\n\n\n",
+			lipgloss.NewStyle().Bold(true).Underline(true).Render("Selected Month"),
+			model.monthSumView(),
+			"\n",
+		)
+}
 
+
+func (model *Model) renderStatisticsView() string {
 	var mainDisplay string
 	var footer string
 	switch model.statsDetails.displayType {
 	case graphDisplay:
 		footer = "[v] Switch View • [Q] Quit"
-		mainDisplay = lipgloss.JoinVertical(
-			lipgloss.Center,
-			model.statsDetails.graphArea.View(),
-			"\n",
-			lipgloss.NewStyle().Bold(true).Underline(true).Render("All time"),
-			allSumFields,
-		)
+		mainDisplay = model.statsGraphView()
 	case tableDisplay:
 		footer = "[v] Switch View • [Up/Down] Traverse Months • [Q] Quit"
-		mainDisplay = lipgloss.JoinVertical(
-			lipgloss.Center,
-			model.statsDetails.table.View(),
-			"\n\n\n\n\n\n\n\n\n",
-			lipgloss.NewStyle().Bold(true).Underline(true).Render("Selected Month"),
-			monthSumFields,
-			"\n",
-		)
+		mainDisplay = model.statsTableView()
 	}
 
 	return lipgloss.Place(
 		model.width,
 		model.height,
 		lipgloss.Center,
-		lipgloss.Center,
+		lipgloss.Top,
 		lipgloss.JoinVertical(
 			lipgloss.Center,
-			header,
+			model.statsHeaderView(),
 			"",
-			yearSelection,
-			topSpacing,
+			model.statsYearSelectionView(),
+			"\n\n",
 			mainDisplay,
 			"\n",
-			model.statusLabel,
-			"\n",
+			lipgloss.NewStyle().Height(2).Render(model.statusLabel),
 			lipgloss.NewStyle().Foreground(lipgloss.Color("238")).Render(footer),
 		),
 	)
